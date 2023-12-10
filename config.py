f53618e3adae2af1e42a83595f6eadf5ca905172
DevAryanxDD <144301993+DevAryanxDD@users.noreply.github.com>
2023-12-10 16:28:41 +0530

    Update config.py
    Signed-off-by: DevAryanxDD <144301993+DevAryanxDD@users.noreply.github.com>

--- config.py
+++ config.py
@@ -44,8 +44,8 @@ SRC_CODE = getenv(
     "https://t.me/DragonBytz",
 )
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/OFFICIAL_91CLUB")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/CLUB91HUBB")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DragonBytz")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+p5sbFQC5igA0OGJl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -67,7 +67,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
-STRING1 = getenv("STRING_SESSION", "BQBebx0AsV7DmjjYBThXeSqYejmdMjhNYlOkRYSNipndYM4iSMyeAPnrSkEzhOgxX16RftSARNw3NI6VCaL_PyeVUs2dJsJVsWjK6rJ1ihSwgCN5bekoXqYuIWzGGDUSBn6PLU7IoALitZ0UTbz5mpYcqV2ygkGNdthOfZp_Bjz9jixHJZ7FTu8aMPCanYgysRi0QB8nBfTeZMTail_Hv186hJEacH4Lnho3j7Cj3TDy0lE4KHzaXT1GEu3VYNazLOiUTrDVXf-6PSG60CgSxgZAJ0e9my3fkPfF2oRc0vXsN-wDJ5qCDInV69HZF4Wp_0ybGJnNYV-xQrT4Wnk6VOrCMlPBMwAAAAA8g0KxAA")
+STRING1 = getenv("STRING_SESSION", "")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -83,10 +83,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/33c46fb32d0b4c4da6bf3.mp4"
+    "START_IMG_URL", "https://telegra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
